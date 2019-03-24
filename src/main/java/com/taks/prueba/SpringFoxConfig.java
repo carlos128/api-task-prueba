68b560a7f287e92203ac7cf2ec3a24922b217710
CarlosMipagina <carlos.mipagina@gmail.com>
2019-03-24 14:50:13 -0500

    semodifica el archivo de configuracion

--- src/main/java/com/taks/prueba/SpringFoxConfig.java
+++ src/main/java/com/taks/prueba/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.task.prueba;
+package com.taks.prueba;
 
 
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/taks/prueba/TaksApplication.java
+++ src/main/java/com/taks/prueba/TaksApplication.java
@@ -1,4 +1,4 @@
-package com.task.prueba;
+package com.taks.prueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
