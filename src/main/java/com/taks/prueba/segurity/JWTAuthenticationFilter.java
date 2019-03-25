f897f954e2b38258bc723f7e0227307979e5d172
CarlosMipagina <carlos.mipagina@gmail.com>
2019-03-25 01:23:10 -0500

    config

--- src/main/java/com/taks/prueba/segurity/JWTAuthenticationFilter.java
+++ src/main/java/com/taks/prueba/segurity/JWTAuthenticationFilter.java
@@ -62,7 +62,7 @@ public class JWTAuthenticationFilter extends  UsernamePasswordAuthenticationFilt
 	                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
 	                .signWith(SignatureAlgorithm.HS512, SECRET)
 	                .compact();
-	        res.addHeader("Access-Control-Allow-Origin", "*");
+	        res.addHeader("Access-Control-Allow-Origin", "http://localhost:4200/");
 	        res.setHeader("Access-Control-Allow-Methods", "POST, GET,  DELETE, PUT");
 	        res.setHeader("Access-Control-Allow-Credentials", "true");
 	        res.setHeader("Access-Control-Max-Age", "3600");
